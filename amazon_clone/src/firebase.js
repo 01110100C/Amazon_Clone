c5ddcf754ab497e3b1e293308d66f2cdd151acb8
Tayler Caufield <taylercaufield@gmail.com>
2026-07-17 05:39:48 -0700

    deleted

--- amazon_clone/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC0TyLmnj-06YR0QS4dXyTS6JmLXPhqfRk",
-  authDomain: "clone-df6c0.firebaseapp.com",
-  projectId: "clone-df6c0",
-  storageBucket: "clone-df6c0.firebasestorage.app",
-  messagingSenderId: "899001973010",
-  appId: "1:899001973010:web:90d9f28b96bbb9ae4ddbcb"
-};
